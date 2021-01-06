cb79c39e5b33c1be335d277c069a8bd27ca37cf2
Finn Willard <finn.willard@gmail.com>
2021-01-06 10:22:15 -0800

    use util constants

--- src/moves.rs
+++ src/moves.rs
@@ -1,6 +1,7 @@
 use crate::board::Board;
 use crate::util::Color;
 use crate::util::shift;
+use crate::util;
 
 struct Move{
     from: u8,
@@ -25,16 +26,14 @@ pub fn pawn_push_targets(pos: Board, side: Color) -> u64 {
 
     match side {
         Color::White => {
-            let rank_4: u64 = 0x00000000ff000000;
             let single_push = shift::north_one(pos.white_pieces.pawns) & empty;
-            let double_push = shift::north_one(single_push) & empty & rank_4;
+            let double_push = shift::north_one(single_push) & empty & util::RANK_4;
 
             return single_push | double_push;
         }
         Color::Black => {
-            let rank_5: u64 = 0x000000ff00000000;
             let single_push = shift::south_one(pos.black_pieces.pawns) & empty;
-            let double_push = shift::south_one(single_push) & empty & rank_5;
+            let double_push = shift::south_one(single_push) & empty & util::RANK_5;
 
             return single_push | double_push;
         }
