4ba2e29041071f319aeee5b1d9ea294f6bd4b291
Finn Willard <finn.willard@gmail.com>
2021-01-08 12:46:04 -0800

    board to FEN function

--- src/board.rs
+++ src/board.rs
@@ -2,6 +2,7 @@ use crate::util;
 use regex::Captures;
 use regex::Regex;
 use std::fmt;
+use std::fmt::Write;
 ///Stores the board state as a set of bitboards
 #[derive(Copy, Clone)]
 pub struct Board {
@@ -30,16 +31,20 @@ impl Board {
         //initialize board with default FEN string
         return Board::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     }
-    ///Initializes a board from a FEN string
+    /// Initializes a board from a FEN string. <br>
+    /// [Click here](https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation) for more info on FEN string structure
     pub fn from(fen: &str) -> Board {
+        //split string into six FEN fields
         let tokens: Vec<&str> = fen.split(" ").collect();
 
+        //get the side to move
         let side = match tokens[1] {
             "w" => util::Color::White,
             "b" => util::Color::Black,
             _ => panic!(),
         };
 
+        //get castling rights
         let mut castle = [false, false, false, false];
 
         if tokens[2].contains("K") {
@@ -55,15 +60,17 @@ impl Board {
             castle[3] = true;
         }
 
+        //check for en passant square
         let ep = match tokens[3] {
             "-" => None,
             _ => Some(util::algebraic_to_index(tokens[3])),
         };
 
+        //get full and half moves
         let half = tokens[4].parse::<u8>().unwrap();
         let full = tokens[5].parse::<u64>().unwrap();
 
-        //generate board layout
+        //generate empty board layout
         let mut w_p: u64 = 0x0000000000000000;
         let mut w_r: u64 = 0x0000000000000000;
         let mut w_n: u64 = 0x0000000000000000;
@@ -94,6 +101,7 @@ impl Board {
         //iterate through all squares of the string, and set the corresponding bit in the correct pieces bitboard
         for (i, c) in stripped.chars().rev().enumerate() {
             match c {
+                // black pieces
                 'p' => b_p = util::set_bit(b_p, i),
                 'r' => b_r = util::set_bit(b_r, i),
                 'n' => b_n = util::set_bit(b_n, i),
@@ -101,6 +109,7 @@ impl Board {
                 'q' => b_q = util::set_bit(b_q, i),
                 'k' => b_k = util::set_bit(b_k, i),
 
+                // white pieces
                 'P' => w_p = util::set_bit(w_p, i),
                 'R' => w_r = util::set_bit(w_r, i),
                 'N' => w_n = util::set_bit(w_n, i),
@@ -108,8 +117,9 @@ impl Board {
                 'Q' => w_q = util::set_bit(w_q, i),
                 'K' => w_k = util::set_bit(w_k, i),
 
-                '.' => (),
-                _ => panic!(),
+                '.' => (), // do nothing for empty squares
+
+                _ => panic!(), // unexpected value
             }
         }
 
@@ -137,6 +147,101 @@ impl Board {
             half_move_clock: half,
             full_move_number: full,
         }
+    }
+
+    pub fn to_fen(&self) -> String {
+        let mut s = String::new();
+        let mut empty_index = 0;
+
+        for i in (0..64).rev() {
+            if !util::bit_is_set(&self.all_pieces(), i) {
+                empty_index += 1;
+            } else {
+                if empty_index > 0 {
+                    write!(s, "{}", empty_index).unwrap();
+                    empty_index = 0;
+                }
+                if util::bit_is_set(&self.black_pieces.pawns, i) {
+                    s.push_str("p");
+                } else if util::bit_is_set(&self.black_pieces.rooks, i) {
+                    s.push_str("r");
+                } else if util::bit_is_set(&self.black_pieces.knights, i) {
+                    s.push_str("n");
+                } else if util::bit_is_set(&self.black_pieces.bishops, i) {
+                    s.push_str("b");
+                } else if util::bit_is_set(&self.black_pieces.queens, i) {
+                    s.push_str("q");
+                } else if util::bit_is_set(&self.black_pieces.king, i) {
+                    s.push_str("k");
+                } else if util::bit_is_set(&self.white_pieces.pawns, i) {
+                    s.push_str("P");
+                } else if util::bit_is_set(&self.white_pieces.rooks, i) {
+                    s.push_str("R");
+                } else if util::bit_is_set(&self.white_pieces.knights, i) {
+                    s.push_str("N");
+                } else if util::bit_is_set(&self.white_pieces.bishops, i) {
+                    s.push_str("B");
+                } else if util::bit_is_set(&self.white_pieces.queens, i) {
+                    s.push_str("Q");
+                } else if util::bit_is_set(&self.white_pieces.king, i) {
+                    s.push_str("K");
+                }
+            }
+
+            if i % 8 == 0 {
+                if empty_index > 0 {
+                    write!(s, "{}", empty_index).unwrap();
+                    empty_index = 0;
+                }
+                if i > 7 {
+                    s.push_str("/");
+                }
+            }
+        }
+
+        s.push(' ');
+
+        match self.side_to_move {
+            util::Color::White => s.push('w'),
+            util::Color::Black => s.push('b'),
+        }
+
+        s.push(' ');
+        let mut flag = true;
+        if self.castle_rights[0] {
+            s.push('K');
+            flag = false;
+        }
+        if self.castle_rights[1] {
+            s.push('Q');
+            flag = false;
+        }
+        if self.castle_rights[2] {
+            s.push('k');
+            flag = false;
+        }
+        if self.castle_rights[3] {
+            s.push('q');
+            flag = false;
+        }
+        if flag {
+            s.push('-');
+        }
+
+        s.push(' ');
+
+        match self.en_passant {
+            None => s.push('-'),
+            Some(sq) => write!(s, "{}", util::index_to_algebraic(sq)).unwrap(),
+        }
+
+        s.push(' ');
+        write!(s, "{}", self.half_move_clock).unwrap();
+
+        s.push(' ');
+        write!(s, "{}", self.full_move_number).unwrap();
+
+        return s;
     }
     ///Returns a bitboard with the location of all the white pieces
     pub fn white_pieces(&self) -> u64 {
@@ -179,29 +284,29 @@ impl fmt::Debug for Board {
         for i in (0..64).rev() {
             write!(f, " ").unwrap();
             if util::bit_is_set(&self.white_pieces.pawns, i) {
-                write!(f, "p").unwrap();
-            } else if util::bit_is_set(&self.white_pieces.rooks, i) {
-                write!(f, "r").unwrap();
-            } else if util::bit_is_set(&self.white_pieces.knights, i) {
-                write!(f, "n").unwrap();
-            } else if util::bit_is_set(&self.white_pieces.bishops, i) {
-                write!(f, "b").unwrap();
-            } else if util::bit_is_set(&self.white_pieces.queens, i) {
-                write!(f, "q").unwrap();
-            } else if util::bit_is_set(&self.white_pieces.king, i) {
-                write!(f, "k").unwrap();
-            } else if util::bit_is_set(&self.black_pieces.pawns, i) {
                 write!(f, "P").unwrap();
-            } else if util::bit_is_set(&self.black_pieces.rooks, i) {
+            } else if util::bit_is_set(&self.white_pieces.rooks, i) {
                 write!(f, "R").unwrap();
-            } else if util::bit_is_set(&self.black_pieces.knights, i) {
+            } else if util::bit_is_set(&self.white_pieces.knights, i) {
                 write!(f, "N").unwrap();
-            } else if util::bit_is_set(&self.black_pieces.bishops, i) {
+            } else if util::bit_is_set(&self.white_pieces.bishops, i) {
                 write!(f, "B").unwrap();
-            } else if util::bit_is_set(&self.black_pieces.queens, i) {
+            } else if util::bit_is_set(&self.white_pieces.queens, i) {
                 write!(f, "Q").unwrap();
-            } else if util::bit_is_set(&self.black_pieces.king, i) {
+            } else if util::bit_is_set(&self.white_pieces.king, i) {
                 write!(f, "K").unwrap();
+            } else if util::bit_is_set(&self.black_pieces.pawns, i) {
+                write!(f, "p").unwrap();
+            } else if util::bit_is_set(&self.black_pieces.rooks, i) {
+                write!(f, "r").unwrap();
+            } else if util::bit_is_set(&self.black_pieces.knights, i) {
+                write!(f, "n").unwrap();
+            } else if util::bit_is_set(&self.black_pieces.bishops, i) {
+                write!(f, "b").unwrap();
+            } else if util::bit_is_set(&self.black_pieces.queens, i) {
+                write!(f, "q").unwrap();
+            } else if util::bit_is_set(&self.black_pieces.king, i) {
+                write!(f, "k").unwrap();
             } else {
                 write!(f, "-").unwrap();
             }
@@ -218,7 +323,7 @@ impl fmt::Debug for Board {
                     5 => writeln!(f, "         En Passant: {}", ep_string).unwrap(),
                     4 => writeln!(f, "    Half-Move Clock: {:?}", self.half_move_clock).unwrap(),
                     3 => writeln!(f, "   Full-Move Number: {:?}", self.full_move_number).unwrap(),
-                    2 => writeln!(f, "                FEN: ").unwrap(),
+                    2 => writeln!(f, "                FEN: {}", self.to_fen()).unwrap(),
                     _ => writeln!(f).unwrap(),
                 }
 
